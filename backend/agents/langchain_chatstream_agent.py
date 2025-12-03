3cbf40184abab0f7f401def49dd808d482780785
Jinsoo-MackbookAir <ljslim27@gmail.com>
2025-12-03 23:39:12 +0900

    refactor( ): code refactoring

--- backend/agents/langchain_chatstream_agent.py
+++ backend/agents/langchain_chatstream_agent.py
@@ -9,6 +9,10 @@ from langchain_classic.chains import ConversationChain
 from langchain_classic.memory import ConversationBufferMemory
 from langchain_community.chat_message_histories import RedisChatMessageHistory
 from backend.core.env_loader import REDIS_HOST, REDIS_PORT, REDIS_DB
+from backend.core.config import settings
+
+OPENAI_API_KEY = settings.OPENAI_API_KEY
+
 
 class QueueCallbackHandler(BaseCallbackHandler):
     """LLM 응답을 Queue에 저장하는 콜백 핸들러"""
@@ -39,7 +43,7 @@ class LangchainChatStreamAgent(BaseAgent):
         q = Queue()
 
         redis_url = f"redis://{REDIS_HOST}:{REDIS_PORT}/{REDIS_DB}"
-        history = RedisChatMessageHistory(chat_id=f"user:{self.user_id}", url=redis_url, ttl=3600)
+        history = RedisChatMessageHistory(session_id=f"user:{self.user_id}", url=redis_url, ttl=3600)
         memory = ConversationBufferMemory(chat_memory=history, return_messages=True)
 
         # 백그라운드 스레드에서 실행될 작업
@@ -50,6 +54,7 @@ class LangchainChatStreamAgent(BaseAgent):
                 temperature=0.3,
                 streaming=True,
                 callbacks=[callback],
+                api_key=OPENAI_API_KEY,
             )
             chain = ConversationChain(llm=llm, memory=memory, verbose=False)
             chain.run(input=user_input)
